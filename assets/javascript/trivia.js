257dd126301fddf24c48e41628c5b10b784de95f
Tokali's <Tokali's>
2016-11-18 15:01:15 -0600

    <adding options>

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
     var gameStarted = false;
+    var right = 0;
+    var wrong = 0;
+    var unanswered = 0;
+
+    var btnColors = ['primary', 'warning', 'primary', 'warning']
     var gameEvents = {
         questionSequence: 0,
         trivia: [{
@@ -8,7 +13,7 @@ $(document).ready(function() {
             answers: ['Pierce Brosnan', 'Roger Moore', 'Sean Connery', 'Timothy Dalton'],
             correctAnswer: 'Sean Connery'
         },{
-            qid: 2,
+            qid: 1,
             question: 'Who star the movie that Dan "the Instructor" James very excited to see',
             answers: ['Demi Moore', 'Scarlett Johansson', 'Rachel McAdams', 'Lady Gaga'],
             correctAnswer: 'Scarlett Johansson'
@@ -17,6 +22,14 @@ $(document).ready(function() {
         play: function() {
             $('#startBtn').empty();
             $('#questionArea').html('<h2>' + this.trivia[this.questionSequence].question) + '</h2>';
+            for (var i = 0; i < this.trivia[this.questionSequence].answers.length; i++) {
+                var option = $('<button>');
+                option.addClass('btn btn-info text-center option');
+                option.attr('data-option', this.trivia[this.questionSequence].answers[i]);
+                option.text(this.trivia[this.questionSequence].answers[i]);
+                $('#options').append(option);
+                $('#options').append('<br>')
+            }
         }
     }
 
@@ -33,6 +46,7 @@ $(document).ready(function() {
     }
     //start button click event
     $('#startBtn').on('click', function() {
+        gameEvents.timer();
         gameEvents.play();
     })
 });
