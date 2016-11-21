ee65b70561d74c4b79e521d26777a7bd4b47da11
Tokali's <Tokali's>
2016-11-20 23:41:54 -0600

    <all done, styling and game functionality>

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -10,25 +10,25 @@ $(document).ready(function() {
         questionSequence: -1, // define the sequence of questions
         // Questions, options and answers array
         trivia: [{
-            qid: 0,
             question: 'who was the first james bond',
             answers: ['Pierce Brosnan', 'Roger Moore', 'Sean Connery', 'Timothy Dalton'],
-            correctAnswer: 'Sean Connery'
+            correctAnswer: 'Sean Connery',
+            image: 'assets/images/james.gif',
         },{
-            qid: 1,
-            question: 'Who star the movie that Dan "the Instructor" James very excited to see',
+            question: 'Who stars the movie that Dan "the Instructor" James is very excited to see',
             answers: ['Demi Moore', 'Scarlett Johansson', 'Rachel McAdams', 'Lady Gaga'],
-            correctAnswer: 'Scarlett Johansson'
+            correctAnswer: 'Scarlett Johansson',
+            image: 'assets/images/scarlett.gif',
         },{
-            qid: 2,
-            question: '3',
-            answers: ['Demi Moore', 'Scarlett Johansson', 'Rachel McAdams', 'Lady Gaga'],
-            correctAnswer: 'Scarlett Johansson'
+            question: 'Who was the first programmer in history',
+            answers: ['Grace Hopper', 'Frances Spence', 'Leah Culver', 'Ada Lovelace'],
+            correctAnswer: 'Ada Lovelace',
+            image: 'assets/images/programmer.gif',
         },{
-            qid: 3,
-            question: '4',
-            answers: ['Demi Moore', 'Scarlett Johansson', 'Rachel McAdams', 'Lady Gaga'],
-            correctAnswer: 'Scarlett Johansson'
+            question: 'The first ',
+            answers: ['Claude Monet', 'Berthe Morisot', 'Edgar Degas', 'Paul Gauguin'],
+            correctAnswer: 'Claude Monet',
+            image: 'assets/images/artist.gif',
         }],
         // 30 sec timer with countdown
         counter: 30,
@@ -55,7 +55,7 @@ $(document).ready(function() {
             $('#questionArea').html('<h2>' + this.trivia[this.questionSequence].question) + '</h2>';
             for (var i = 0; i < this.trivia[this.questionSequence].answers.length; i++) {
                 var option = $('<button>');
-                option.addClass('btn btn-default text-center answer');
+                option.addClass('btn text-center answer');
                 option.attr('data-option', this.trivia[this.questionSequence].answers[i]);
                 option.text(this.trivia[this.questionSequence].answers[i]);
                 $('#options').append(option);
@@ -75,7 +75,8 @@ $(document).ready(function() {
                 $('#resultsBox').empty();
                 gameEvents.countStop();
                 gameEvents.countReset();
-                $('#resultsBox').html('<h2>Correct!</h2>');
+                $('#resultsBox').append('<h2>Correct!</h2>');
+                $('#resultsBox').append('<img src='+ this.trivia[this.questionSequence].image +' />');
                 timeUp = setTimeout(gameEvents.nextQuestion, 4000)
             } else {
                 clearTimeout(timeUp);
@@ -86,10 +87,13 @@ $(document).ready(function() {
                 $('#resultsBox').empty();
                 gameEvents.countStop();
                 gameEvents.countReset();
-                $('#resultsBox').html('<h2>Wrong!</h2>');
+                $('#resultsBox').append('<h2>Wrong!</h2>');
+                $('#resultsBox').append('<h4>The right answer is: <span>' + this.trivia[this.questionSequence].correctAnswer + '</span></h4>');
+                $('#resultsBox').append('<img src='+ this.trivia[this.questionSequence].image +' />');
                 timeUp = setTimeout(gameEvents.nextQuestion, 4000)
             }
         },
+        // below function called when 30 seconds end without clicking
         timeIsUp: function() {
             clearTimeout(timeUp);
             unanswered++;
@@ -99,7 +103,9 @@ $(document).ready(function() {
             $('#resultsBox').empty();
             gameEvents.countStop();
             gameEvents.countReset();
-            $('#resultsBox').html('<h2>Time is Up!!!</h2>');
+            $('#resultsBox').append('<h2>Time is Up!!!</h2>');
+            $('#resultsBox').append('<h4>The right answer is: <span>' + gameEvents.trivia[gameEvents.questionSequence].correctAnswer + '</span></h4>');
+            $('#resultsBox').append('<img src='+ gameEvents.trivia[gameEvents.questionSequence].image +' />');
             timeUp = setTimeout(gameEvents.nextQuestion, 4000)
         },
         // Next action: 1) go to next question if any left or go to the final results board
@@ -120,12 +126,17 @@ $(document).ready(function() {
             $('#resultsBox').empty();
             $('#resultsBox').append('<h3> Right Answers: ' + right + '</h3>');
             $('#resultsBox').append('<h3> Wrong Answers: ' + wrong + '</h3>');
+            $('#resultsBox').append('<h3> Unanswered: ' + unanswered + '</h3>');
+            var playAgain = $('<button>');
+            playAgain.text('Play Again??');
+            playAgain.addClass('btn resetBtn');
+            $('#resultsBox').append(playAgain);
         },
     }
     // display Start button on page load only
     function startBtn() {
         var startBtn = $('<button>');
-        startBtn.addClass('btn btn-default startBtn');
+        startBtn.addClass('btn startBtn');
         startBtn.text('START THE TRIVIA');
         $('#startBtn').append(startBtn);
     }
@@ -134,6 +145,15 @@ $(document).ready(function() {
     }
     //start button click event, start the questions and timer
     $('#startBtn').on('click', function() {
+        gameEvents.nextQuestion();
+    });
+    //
+    $('.resetBtn').on('click', function() {
+        $('#startBtn').empty();
+        $('#questionArea').empty();
+        $('#options').empty();
+        $('#resultsBox').empty();
+        gameEvents.questionSequence = -1
         gameEvents.nextQuestion();
     });
     // click event listener when click on one of the answers
